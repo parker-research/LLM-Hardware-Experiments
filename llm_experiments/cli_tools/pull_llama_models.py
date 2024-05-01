66aa9ee9bab4d6de44358cc1addd2a6a7c270cf0
parker-research <166864283+parker-research@users.noreply.github.com>
2024-04-30 22:30:50 -0600

    Add model pull tool

---
+++ llm_experiments/cli_tools/pull_llama_models.py
@@ -0,0 +1,42 @@
+"""\
+A quick tool to ensure a host of Llama models are downloaded and ready to use.
+"""
+
+import argparse
+
+from loguru import logger
+
+from llm_experiments.llms.models.ollama_llm import OllamaLlm, OllamaLlmConfig
+
+
+def pull_ollama_model(model_name: str) -> None:
+    logger.info(f"Pulling model: {model_name}")
+    config = OllamaLlmConfig(model_name=model_name)
+    llm = OllamaLlm("ollama", config)
+    llm.pull()
+
+    logger.info(f"Model {model_name} is ready to use.")
+
+
+def main_cli():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "-m",
+        "--model",
+        dest="model_names",
+        action="append",
+        required=True,
+        help="The model names to pull (e.g., 'codellama:34b-instruct').",
+    )
+    args = parser.parse_args()
+
+    logger.info(f"Pulling the following {len(args.model_names)} models: {args.model_names}")
+
+    for model_name in args.model_names:
+        pull_ollama_model(model_name)
+
+    logger.info("All models are ready to use.")
+
+
+if __name__ == "__main__":
+    main_cli()
